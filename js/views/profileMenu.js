245c206e37f2260280e6e1f7b56560cdff96e92c
Qwiplus <sniperdetroit@gmail.com>
2026-05-10 17:53:08 +0200

    fix: lang localStorage fallback

--- js/views/profileMenu.js
+++ js/views/profileMenu.js
@@ -1,6 +1,7 @@
 // js/views/profileMenu.js
 
 import { navigate } from "../core/router.js";
+import { t } from "../services/langService.js";
 
 /* ================= RENDER ================= */
 
@@ -10,28 +11,24 @@ function renderProfileMenu(){
   <section class="profile-menu">
 
     <div class="profile-top">
-      <button id="backProfileMenu">← Volver</button>
+      <button id="backProfileMenu">← ${t("back")}</button>
     </div>
 
-    <h2>Mi cuenta</h2>
+    <h2>${t("myAccount")}</h2>
 
     <div class="profile-menu-list">
 
       <button class="profile-menu-btn" data-view="profile">
-        👤 Ver perfil
+        👤 ${t("viewProfile")}
       </button><br>
 
       <button class="profile-menu-btn" data-view="favorites">
-        ❤️ Favoritos
+        ❤️ ${t("favorites")}
       </button><br>
 
       <button class="profile-menu-btn" data-view="settings">
-        ⚙️ Configuración
-      </button> 
-
-     
-
-      
+        ⚙️ ${t("config")}
+      </button>
 
     </div>
 
@@ -59,3 +56,4 @@ export const ProfileMenuView = () => {
     mount: mountProfileMenu
   };
 };
+
